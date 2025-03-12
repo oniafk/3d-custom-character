1ccefc95e4f38c678f756ec46041ec375a646654
oniafk <manufelipearias@gmail.com>
2025-03-12 12:37:46 +1100

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { UI } from "./components/UI";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Leva } from "leva";
+import { DEFAULT_CAMERA_POSITION } from "./components/CameraManager";
 import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
 
 function App() {
   return (
     <>
+      <Leva hidden />
       <UI />
-      <Canvas camera={{ position: [3, 3, 3] }}>
-        <color attach="background" args={["lightblue"]} />
-        <Experience />
+      <Canvas
+        camera={{
+          position: DEFAULT_CAMERA_POSITION,
+          fov: 45,
+        }}
+        gl={{
+          preserveDrawingBuffer: true,
+        }}
+        shadows
+      >
+        <color attach="background" args={["#130f30"]} />
+        <fog attach="fog" args={["#130f30", 10, 40]} />
+        <group position-y={-1}>
+          <Experience />
+        </group>
+        <EffectComposer>
+          <Bloom mipmapBlur luminanceThreshold={1.2} intensity={1.2} />
+        </EffectComposer>
       </Canvas>
     </>
   );
